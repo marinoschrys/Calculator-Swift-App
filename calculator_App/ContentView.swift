27c4cab5d02b4e278189f646d9881c6c2432ffc8
Marinos Chrysostomou <98773997+marinoschrys@users.noreply.github.com>
2026-09-24 18:29:59 +0300

    Added repeating function

--- calculator_App/ContentView.swift
+++ calculator_App/ContentView.swift
@@ -8,60 +8,118 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var number1 = ""
-    @State private var number2 = ""
-    @State private var selectedOperator = ""
-    @State private var result = ""
+    @State private var display = "0"
+    @State private var previewsNumber = ""
+    @State private var currentOperation = ""
+    @State private var lastOperand: Int? = nil
+    @State private var isRepeating = false
     
-    let operators = ["+", "-", "*", "/"]
-    
+    let buttons = [
+        ["7", "8", "9", "/"],
+        ["4", "5", "6", "*"],
+        ["1", "2", "3", "-"],
+        ["C", "0", "=", "+"]
+    ]
+
     var body: some View {
-        VStack (spacing: 20){
-            TextField("First Number", text: $number1)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            
-            Picker("Operation", selection: $selectedOperator) {
-                ForEach(operators, id: \.self) { Text($0) }
+        VStack (spacing: 15) {
+            Text(display)
+                .font(.system(size: 50))
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .padding()
+
+            ForEach(buttons, id: \.self) { row in
+                HStack (spacing: 12){
+                    ForEach(row, id: \.self) { buttonTitle in
+                        Button(action: {
+                            buttonTapped(buttonTitle)
+                        }) {
+                            Text(buttonTitle)
+                                .font(.title)
+                                .frame(width: 70, height: 70)
+                                .background(getBackgroundColor(buttonTitle))
+                                .clipShape(.circle)
+                                .foregroundColor(.white)
+                        }
+                        .frame(width: 70, height: 70)
+                        .buttonStyle(.plain)
+                    }
+                }
             }
-            .pickerStyle(.segmented)
-            
-            TextField("Second Number", text: $number2)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            
-            Button("Calculate") {
-                calculate()
-            }
-            .buttonStyle(.borderedProminent)
-            
-            Text(result)
-                .font(.title)
         }
         .padding()
     }
     
-    func calculate() {
-        guard let num1 = Int(number1), let num2 = Int(number2) else {
-            result = "Error: Invalid Input"
-            return
+    func getBackgroundColor(_ title: String) -> Color{
+        switch title {
+        case "C":
+            return .red
+        case "=":
+            return .indigo
+        case "+", "-", "/", "*":
+            return .orange
+        default:
+            return Color.gray.opacity(0.2)
+        }
+    }
+    
+    func buttonTapped(_ title: String) {
+        switch title {
+        case "C":
+            display = "0"
+            previewsNumber = ""
+            currentOperation = ""
+            isRepeating = false
+        case "=": calculateResult()
+        case "+","-","*","/":
+            previewsNumber = display
+            currentOperation = title
+            display = "0"
+        default:
+            if display == "0" {
+                display = title
+            }
+            else {
+                display += title
+            }
+        }
+    }
+    
+    func calculateResult() {
+        
+        let num2: Int
+        if isRepeating, let last = lastOperand {
+            num2 = last
+        } else {
+            guard let n2 = Int(display) else { return }
+            num2 = n2
+            lastOperand = num2
         }
         
-        var calculationResult: Int?
+        guard let num1 = Int(previewsNumber) else { return }
         
-        switch selectedOperator {
-        case "+": calculationResult = num1 + num2
-        case "-": calculationResult = num1 - num2
-        case "*": calculationResult = num1 * num2
-        case "/": calculationResult = num2 != 0 ? num1/num2: nil
+        var result = 0
+        
+        switch currentOperation {
+        case "+": result = num1 + num2
+        case "-": result = num1 - num2
+        case "*": result = num1 * num2
+        case "/":
+            if num2 != 0 {
+                result = num1/num2
+            }
+            else {
+                display = "Error"
+                return
+            }
         default: break
         }
         
-        if calculationResult != nil {
-            result = "Result =  \(calculationResult!)"
-        }
-        else {
-            result = "Error"
-        }
+        display = String(result)
+        previewsNumber = String(result)
+        isRepeating = true
     }
+    
 }
 
 #Preview {
